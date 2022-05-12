45a92eed1ec494272525ed26401d77285000c840
Dylan Workman <st20156395@outlook.cardiffmet.ac.uk>
2022-05-12 22:18:48 +0100

    first commit, ui changes

---
+++ Assets/Scripts/UI Scripts/ScoreManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    public GameObject scoreText;
+    public static int theScore;
+
+    void Update(){
+        scoreText.GetComponent<Text>().text = "Score: " + theScore;
+    }
+}
